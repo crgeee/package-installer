b8208e1946649c5ce7e92d4aefa378d378d482ef
crgeee <macbookpro@Chris-Gs-MBP.local>
2017-06-06 14:23:08 -0700

    add initial groundwork for installer and tests

---
+++ js/packageInstaller.js
@@ -0,0 +1,11 @@
+function PackageInstaller() {
+  return {
+    install: install
+  };
+
+  function install() {
+    
+  }
+}
+
+module.exports = PackageInstaller();
---
+++ test/packageInstaller.spec.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var packageinstaller = require('../js/packageinstaller.js');
+var should = require('chai').should();
+
+describe('PackageInstaller', function () {
+  describe('', function() {
+
+  })
+})

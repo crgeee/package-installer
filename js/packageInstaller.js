abd0cbd5d79f2f2a2b3caf311d9c391758250d83
crgeee <macbookpro@Chris-Gs-MBP.local>
2017-06-06 14:43:15 -0700

    add validation checks for packages to install

--- js/packageInstaller.js
+++ js/packageInstaller.js
@@ -3,8 +3,32 @@ function PackageInstaller() {
     install: install
   };
 
-  function install() {
-    
+  /**
+   * @function install
+   * @desc Parses and returns packages as a string.
+   * @param {Array} packages - Input array of packages as Package: Dependency strings for installation.
+   * @returns {string} packages result
+   */
+  function install(packages) {
+    if (packages == null) {
+      throw 'Packages are required.';
+    }
+
+    if (!Array.isArray(packages)) {
+      throw 'Packages must be of type array.';
+    }
+
+    packages.forEach(function(pkg) {
+      if (typeof pkg !== 'string') {
+        throw 'All packages must be of type string.';
+      }
+      if (pkg.indexOf(':') < 1) {
+        //todo change indexOf to regEx pattern matching
+        throw 'Package does not match expected format of \'Package: Depdency\' ';
+      }
+    });
+
+    return packages;
   }
 }
 
--- test/packageInstaller.spec.js
+++ test/packageInstaller.spec.js
@@ -1,10 +1,15 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
-var packageinstaller = require('../js/packageinstaller.js');
+var packageInstaller = require('../js/packageinstaller.js');
 var should = require('chai').should();
 
 describe('PackageInstaller', function () {
-  describe('', function() {
-
+  describe('install', function() {
+    it('should return error \'Packages are required.\'', function() {
+      var input = null;
+      expect(function() {
+        packageInstaller.install(input);
+      }).to.throw('Packages are required.');
+    })
   })
 })
